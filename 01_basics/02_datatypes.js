b7c06159fef732d5f373f0596a70165c64d8fc3c
shourya kumar jha <54201069+skjha123@users.noreply.github.com>
2025-03-03 06:28:05 +0000

    commit

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+ "use strict";// treat all code as newer version;
+ //alert("hello");// we are using node js not browser;
+
+
+ let name="Abdul";
+ let age=18;
+ let isLoggedIn=true;
+ console.log(typeof "abdul");
+ console.log(typeof null);
+ console.log(typeof undefined);
+ 
+ 
