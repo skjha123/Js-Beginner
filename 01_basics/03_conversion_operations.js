c589fe350f351a59de92cd7198c9bd02471a70b8
shourya kumar jha <54201069+skjha123@users.noreply.github.com>
2025-03-03 09:58:29 +0000

    changed succesfully

--- 01_basics/03_conversion_operations.js
+++ 01_basics/03_conversion_operations.js
@@ -1,12 +1,55 @@
-let score="33";
-console.log(typeof score);
-console.log(typeof(score));
-let valueInNumber=Number(score);
-console.log(typeof valueInNumber);
+//let score="33";
+// console.log(typeof score);
+// console.log(typeof(score));
+// let valueInNumber=Number(score);
+// console.log(typeof valueInNumber);
 
-let isLoggedin="shourya";
-let booleanloggedin=Boolean(isLoggedin);
-console.log(booleanloggedin);
+// let isLoggedin="shourya";
+// let booleanloggedin=Boolean(isLoggedin);
+// console.log(booleanloggedin);
+
+
+//******************************operations */
+
+// let value=3;
+// let negvalue=-value;
+
+// console.log(negvalue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+
+
+str1="hello";
+str2="kumar";
+
+let str3=str1+" "+str2;
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+
+
+// console.log(+true);
+
+// let num1,num2,num3;
+
+// num1=num2=num3=2+2;
+
+let gameCounter=100;
+++gameCounter;
+console.log(gameCounter);
+
+
+
 
 
 
---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,11 @@
+// console.log(null==0);
+// console.log(null>=0);
+
+// console.log(undefined==0);
+// console.log(undefined<0);
+// console.log(undefined>0);
+
+
+//=== used to chekc values and datatype
+
+console.log("2"===2);
