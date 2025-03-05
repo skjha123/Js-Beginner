716f1de9b3bdc21fc964a825012467226af5835c
shourya kumar jha <54201069+skjha123@users.noreply.github.com>
2025-03-05 18:08:50 +0000

    changed successfully

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,36 @@
+const score=400;
+//console.log(score);
+
+const balance=new Number(100);
+
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherName=123.8966;
+//console.log(otherName.toPrecision(3));
+const hundreds=1000000;
+//console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++++++++++++++++++++++++++++++++Maths+++++++++++++++++++++
+console.log(Math);
+
+// console.log(Math.abs(-4));
+// console.log(Math.ceil(4.2));
+// console.log(Math.round(4.5));
+// console.log(Math.floor(4.8));
+// console.log(Math.min(1,23,47,0));
+
+let val=Math.random()*10;
+console.log(Math.floor(val));
+
+const min=10;
+const max=20;
+
+console.log(Math.floor((Math.random()*(max-min+1))+min));
+
+
+
+
+
+
