881b17733dba902d8b2cb7db3a26f740366f38f7
shourya kumar jha <54201069+skjha123@users.noreply.github.com>
2025-03-14 19:26:27 +0000

    chnaged successfully

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -23,6 +23,41 @@ function loginUserMesage(username="sam"){
 }
 
 const message=loginUserMesage();
-console.log(message);
+//console.log(message);
 
 
+//using rest operator three dots (...)
+function calculateCartPrice(val1,val2,...num1){
+return num1;
+}
+
+console.log(calculateCartPrice(200,400,500,1000));
+
+const user={
+    username:"hitesh",
+    price:199
+}
+
+function handleObject(anyobject){
+console.log(`username is ${anyobject.username} and price is ${anyobject.price} `);
+
+}
+
+//handleObject(user);
+
+//wqe also direct pass object in function
+handleObject({
+    username:"sam",
+    price:300
+});
+
+const myNewArray=[200,400,600];
+function returnSecondValue(getArray){
+    return getArray[1];
+
+}
+
+//console.log(returnSecondValue(myNewArray));
+//we can also pass direct array in function
+
+console.log(returnSecondValue([10,20,30]));
