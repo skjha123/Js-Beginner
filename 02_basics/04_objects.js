80831783f32c7e1ede4277d9af3b4a6a9e4b0dc2
shourya kumar jha <54201069+skjha123@users.noreply.github.com>
2025-03-14 15:00:34 +0000

    changed successfully

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -22,12 +22,40 @@ const obj2={3:"a",4:"b"};
 const obj3={...obj1,...obj2};
 //console.log(obj3);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));//convets every key values into array
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));//convets every key values into array
 
-console.log(tinderUser.hasOwnProperty("isloggedIn"));
+//console.log(tinderUser.hasOwnProperty("isloggedIn"));
 
+/* Destructuring of objects */
+
+const course={
+ coursename:"js in hindi",
+ prince: "999",
+ courseInstructure:"Histesh"
+};
+
+const {courseInstructure:instructor}=course; ///destructure 
+console.log(instructor);
+
+
+//API FORMS
+
+/*
+we receive API in JSON format
+
+1. in the form of {
+"name ":"hitesh",
+"coursename":"js",
+"price":"free"
+}
+
+2.in the form of [
+{},
+{}
+]
+*/
 
 
 
