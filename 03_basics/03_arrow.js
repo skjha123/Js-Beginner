353979e46b960d40f71eed82e0a2134756eb6faa
shourya kumar jha <54201069+skjha123@users.noreply.github.com>
2025-03-16 07:22:56 +0000

    changed successfully

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,52 @@
+const user={
+    username:'jhon',
+    price:999,
+    welcomeMessage:function(){
+        console.log(`${this.username} welcome ot website`);
+        console.log(this);
+        
+        
+    }
+}
+
+// user.welcomeMessage();
+// user.username="sam";
+// user.welcomeMessage();
+
+//console.log(this);
+// this doesn't work inside the function
+// function chai(){
+//     let username="jhon";
+//     console.log(this.username);
+    
+// }
+// chai();
+
+
+const chai=()=>{
+    let username="sam";
+}
+
+const res=(num1,num2)=>{
+return num1+num2;
+}
+
+console.log(res(2,3));
+
+
+const sub=(num1,num2)=> num1-num2;// implicit return possible if we write only one line 
+//explicit return mea we are writing return keyword
+
+console.log(sub(4,3));
+
+
+//if you want to return onject then need to wrap inside the ().
+const addtwo=(num1,num2)=>({user:"peter"});
+console.log(addtwo(3,4));
+
+
+//IIFE-Immediately invoked function expression
+
+
+
+
