10e41bf98916524d891baa6061c533d52ac2f021
shourya kumar jha <54201069+skjha123@users.noreply.github.com>
2025-03-16 07:32:13 +0000

    chnaged successfully

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -31,22 +31,32 @@ const res=(num1,num2)=>{
 return num1+num2;
 }
 
-console.log(res(2,3));
+//console.log(res(2,3));
 
 
 const sub=(num1,num2)=> num1-num2;// implicit return possible if we write only one line 
 //explicit return mea we are writing return keyword
 
-console.log(sub(4,3));
+//console.log(sub(4,3));
 
 
 //if you want to return onject then need to wrap inside the ().
 const addtwo=(num1,num2)=>({user:"peter"});
-console.log(addtwo(3,4));
+//console.log(addtwo(3,4));
 
 
 //IIFE-Immediately invoked function expression
 
 
+//names iffe
+(function multiply(){
+    console.log(3*10);
+    
+})();
+
+((name)=>{
+    console.log(`my name is ${name}`);
+    
+})("Tom");
 
 
